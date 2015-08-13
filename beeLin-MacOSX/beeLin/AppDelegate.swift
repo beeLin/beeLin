040d1f9aeaef2ae7360fe694c009d73f96c14cad
Sean Gilligan <sean@msgilligan.com>
2015-08-12 17:07:11 -0700

    Create new Mac OS X project using Xcode app template

---
+++ beeLin-MacOSX/beeLin/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  beeLin
+//
+//  Created by Sean Gilligan on 8/12/15.
+//  Copyright (c) 2015 Sean Gilligan & Genevieve Primavera. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ beeLin-MacOSX/beeLin/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  beeLin
+//
+//  Created by Sean Gilligan on 8/12/15.
+//  Copyright (c) 2015 Sean Gilligan & Genevieve Primavera. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
